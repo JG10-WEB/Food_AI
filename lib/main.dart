1f209d0fe4824f0f3c436e72b6e4ae4f73f32aca
Jay Goyal <74010834+JG10-WEB@users.noreply.github.com>
2022-02-07 17:49:36 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fruit Recognition',
+      title: 'Food Recognition',
       home: Home(),
       debugShowCheckedModeBanner: false,
     );
